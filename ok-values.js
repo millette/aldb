8c8eef61dde503e2c81cf45b7432b9632e463e57
Robin Millette <robin@millette.info>
2019-07-04 07:01:03 -0400

    Output data on jsons that couldn be parsed.

---
+++ ok-values.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// npm
+const level = require('level')
+
+const db = level('dbdbdoo', { valueEncoding: 'json' })
+
+const doit = async () => {
+  const stream = db.createReadStream({
+    gt: "file:"
+  })
+  for await (const { key, value } of stream) {
+    const fn = 'collections/' + key.split(':').slice(1).join(':') + '.json'
+    console.log(JSON.stringify(
+      {
+        fn,
+        value
+      },
+      null,
+      "  "
+    ))
+  }
+}
+
+doit().then(console.log).catch(console.error)
